8145e4d4b23185aa02bcb82b9d3bf8651acbec68
Christian <kristian2015ok@gmail.com>
2024-06-14 22:50:12 +0700

    Perf: use RESTful API to display notes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,23 @@
+import { useEffect, useState } from "react";
 import Note from "../../components/Note/Note";
-import { notes } from "../../data/notes";
 import "./Home.css";
 
 const Home = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://notes-api.dicoding.dev/v2/notes`)
+      .then((res) => res.json())
+      .then((res) => setNotes(res.data));
+  }, []);
+
   return (
     <main className="container mt-lg-5 mb-5 mt-3">
       <h1 className="text-center">All Notes</h1>
       <div className="notes-container row row-gap-3 mt-lg-5 mt-3">
-        {notes.map((note) => (
-          <Note note={note}  key={note.id} />
+        {!notes.length && <p className="text-center text-white fs-5 fw-semibold">No Notes...</p>}
+        {notes.length && notes.map((note) => (
+          <Note note={note} key={note.id} />
         ))}
       </div>
     </main>
