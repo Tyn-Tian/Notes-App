00caa70e9a0f75d7326120062d2d71965695ff02
Christian <kristian2015ok@gmail.com>
2024-06-17 20:13:51 +0700

    Test: OutlineButton Components

--- src/components/OutlineButton/__test__/OutlineButton.test.js
+++ src/components/OutlineButton/__test__/OutlineButton.test.js
@@ -1,9 +1,56 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import OutlineButton from "../OutlineButton";
 import "@testing-library/jest-dom";
 
-it("should render a button", async () => {
-  render(<OutlineButton />);
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toBeInTheDocument();
+const mockedHandleClick = jest.fn();
+
+describe("OutlineButton Component", () => {
+  it("should render a button", async () => {
+    render(
+      <OutlineButton
+        desc="Test"
+        className="primary"
+        handleClick={mockedHandleClick}
+      />
+    );
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("should render correct description", () => {
+    render(
+      <OutlineButton
+        desc="Test"
+        className="primary"
+        handleClick={mockedHandleClick}
+      />
+    );
+    const buttonElement = screen.getByText(/Test/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("should has the correct classes", () => {
+    render(
+      <OutlineButton
+        desc="Test"
+        className="primary"
+        handleClick={mockedHandleClick}
+      />
+    );
+    const buttonElement = screen.getByText(/Test/i);
+    expect(buttonElement).toHaveClass("btn btn-lg btn-outline-primary");
+  });
+
+  it('should calls handleClick when clicked', () => {
+    render(
+      <OutlineButton
+        desc="Test"
+        className="primary"
+        handleClick={mockedHandleClick}
+      />
+    );
+    const buttonElement = screen.getByText(/Test/i);
+    fireEvent.click(buttonElement);
+    expect(mockedHandleClick).toHaveBeenCalledTimes(1);
+  });
 });
