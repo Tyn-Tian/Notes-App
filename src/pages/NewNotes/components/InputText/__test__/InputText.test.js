c4fe7bbca0ef554f000360b4af8112a8ca069bcb
Christian <kristian2015ok@gmail.com>
2024-06-17 20:44:44 +0700

    Test: InputText Components

--- src/pages/NewNotes/components/InputText/__test__/InputText.test.js
+++ src/pages/NewNotes/components/InputText/__test__/InputText.test.js
@@ -1,11 +1,60 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import InputText from "../InputText";
 import "@testing-library/jest-dom";
 
-it('should render label and input element', () => {
-    render(<InputText label="test" placeholder="test" />);
+const mockedHandleChange = jest.fn();
+
+describe("InputText Component", () => {
+  it("should render with correct label", () => {
+    render(
+      <InputText
+        label="test"
+        placeholder="test"
+        value=""
+        handleChange={mockedHandleChange}
+      />
+    );
     const labelElement = screen.getByLabelText(/test/i);
-    const inputElement = screen.getByPlaceholderText(/test/i);
     expect(labelElement).toBeInTheDocument();
-    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should render with correct placeholder", () => {
+    render(
+      <InputText
+        label="test"
+        placeholder="test"
+        value=""
+        handleChange={mockedHandleChange}
+      />
+    );
+    const inputElement = screen.getByPlaceholderText(/test/i);
+    expect(inputElement).toHaveAttribute("placeholder", "test");
+  });
+
+  it("should render with correct value", () => {
+    render(
+      <InputText
+        label="test"
+        placeholder="test"
+        value="test"
+        handleChange={mockedHandleChange}
+      />
+    );
+    const inputElement = screen.getByPlaceholderText(/test/i);
+    expect(inputElement).toHaveAttribute("value", "test");
+  });
+
+  it("should calls handleChange on input change", () => {
+    render(
+      <InputText
+        label="test"
+        placeholder="test"
+        value="test"
+        handleChange={mockedHandleChange}
+      />
+    );
+    const inputElement = screen.getByPlaceholderText(/test/i);
+    fireEvent.change(inputElement, { target: { value: "Changed" } });
+    expect(mockedHandleChange).toHaveBeenCalledTimes(1);
+  });
 });
