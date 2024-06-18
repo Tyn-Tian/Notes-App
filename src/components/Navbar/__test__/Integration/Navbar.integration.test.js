9539740c0aa57ee66085b09fe6d0698f25b94459
Christian <kristian2015ok@gmail.com>
2024-06-18 17:16:52 +0700

    Test: separating testing into unit and integration

---
+++ src/components/Navbar/__test__/Integration/Navbar.integration.test.js
@@ -0,0 +1,69 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "../../Navbar";
+import "@testing-library/jest-dom";
+
+const checkNavItem = (navItems) => {
+  navItems.map((navItem) => {
+    const element = screen.getByText(navItem.desc);
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveAttribute("href", navItem.href);
+    return true;
+  });
+};
+
+const checkNavItemWhenClicked = (navItems) => {
+  navItems.map((navItem) => {
+    fireEvent.click(screen.getByText(navItem.desc));
+    expect(window.location.pathname).toBe(navItem.to);
+    return true;
+  })
+}
+
+describe("Navbar Component Integration Test", () => {
+  it("should render navigation and NavItem component", () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    const navElement = screen.getByRole("navigation");
+    expect(navElement).toBeInTheDocument();
+    checkNavItem([
+      {
+        desc: /Home/i,
+        href: "/",
+      },
+      {
+        desc: /New Notes/i,
+        href: "/notes",
+      },
+      {
+        desc: /Archived Notes/i,
+        href: "/notes/archived",
+      },
+    ]);
+  });
+
+  it('should navigates to correct page when NavItem is clicked', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    checkNavItemWhenClicked([
+      {
+        desc: /Home/i,
+        to: "/",
+      },
+      {
+        desc: /New Notes/i,
+        to: "/notes",
+      },
+      {
+        desc: /Archived Notes/i,
+        to: "/notes/archived",
+      },
+    ]);
+  });
+});
---
+++ src/components/Navbar/__test__/Unit/Navbar.test.js
@@ -0,0 +1,61 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "../../Navbar";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+jest.mock("../../components/NavItem", () => ({ desc, to }) => (
+  <a href={to}>{desc}</a>
+));
+
+const checkNavItem = (navItems) => {
+  navItems.map((navItem) => {
+    const element = screen.getByText(navItem.desc);
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveAttribute("href", navItem.href);
+    return true;
+  });
+};
+
+describe("Navbar Component", () => {
+  it("should render a navigation", () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    const navElement = screen.getByRole("navigation");
+    expect(navElement).toBeInTheDocument();
+  });
+
+  it("should render NavItem with correct description and href", () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    checkNavItem([
+      {
+        desc: /Home/i,
+        href: "/",
+      },
+      {
+        desc: /New Notes/i,
+        href: "/notes",
+      },
+      {
+        desc: /Archived Notes/i,
+        href: "/notes/archived",
+      },
+    ]);
+  });
+
+  it("should render navbar toggler button", () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    const togglerButton = screen.getByLabelText(/Toggle navigation/i);
+    expect(togglerButton).toBeInTheDocument();
+  });
+});
