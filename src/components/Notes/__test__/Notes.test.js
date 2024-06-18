6b1f1b2b5c68a9c9cb2b80974748415e7a0dd76f
Christian <kristian2015ok@gmail.com>
2024-06-18 15:08:56 +0700

    Test: Notes Components

---
+++ src/components/Notes/__test__/Notes.test.js
@@ -0,0 +1,70 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Notes from "../Notes";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+
+const mockData = [
+  {
+    id: "1",
+    title: "Title One",
+    createdAt: "2023-01-01T00:00:00.000Z",
+    body: "Body One",
+  },
+  {
+    id: "2",
+    title: "Title Two",
+    createdAt: "2024-01-01T00:00:00.000Z",
+    body: "Body Two",
+  }
+];
+
+describe("Notes Component", () => {
+  it("should render loading message when isLoading prop is true", () => {
+    render(<Notes isLoading={true} error="null" data="null" />);
+    const parafElement = screen.getByText(/Loading.../i);
+    expect(parafElement).toBeInTheDocument();
+  });
+
+  it("should render error message when error prop is not null", () => {
+    render(
+      <Notes isLoading={false} error={{ message: "Error" }} data="null" />
+    );
+    const parafElement = screen.getByText(/Error/i);
+    expect(parafElement).toBeInTheDocument();
+  });
+
+  it("should render Note Component when data prop is not null", () => {
+    render(
+      <BrowserRouter>
+        <Notes isLoading={false} error={null} data={mockData} />
+      </BrowserRouter>
+    );
+    const NoteComponent = screen.getByText(/Title One/i);
+    expect(NoteComponent).toBeInTheDocument();
+  });
+
+  it("should redirect when Note Components clicked", () => {
+    render(
+      <BrowserRouter>
+        <Notes isLoading={false} error={null} data={mockData} />
+      </BrowserRouter>
+    );
+    const cardElements = screen.getAllByTestId("note-card");
+    cardElements.map((card, index) => {
+      fireEvent.click(card);
+      return expect(window.location.pathname).toBe(`/notes/${mockData[index].id}`);
+    })
+  });
+
+  it("should render multiple Note Components when have multiple data notes", () => {
+    render(
+      <BrowserRouter>
+        <Notes isLoading={false} error={null} data={mockData} />
+      </BrowserRouter>
+    );
+    const NoteComponentOne = screen.getByText(/Title One/i);
+    const NoteComponentTwo = screen.getByText(/Title Two/i);
+    expect(NoteComponentOne).toBeInTheDocument();
+    expect(NoteComponentTwo).toBeInTheDocument();
+  });
+});
