9ab10a408b8db3e18699287bd0336304c56bdf01
Christian <kristian2015ok@gmail.com>
2024-06-20 11:08:10 +0700

    Refactor: hook test location

--- src/__test__/hooks/useFetch.test.js
+++ src/__test__/hooks/useFetch.test.js
@@ -1,5 +1,5 @@
 import { renderHook, waitFor } from "@testing-library/react";
-import useFetch from "../../../hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 
 const mockApiService = jest.fn();
 
