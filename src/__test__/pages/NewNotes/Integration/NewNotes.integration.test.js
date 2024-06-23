aafee48c362ff6cc390982cad3f5ee1025f38beb
Christian <kristian2015ok@gmail.com>
2024-06-23 13:42:25 +0700

    Test: test integration NewNotes Component

---
+++ src/__test__/pages/NewNotes/Integration/NewNotes.integration.test.js
@@ -0,0 +1,83 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import NewNotes from "../../../../pages/NewNotes/NewNotes";
+import apiService from "../../../../services/api.service";
+import "@testing-library/jest-dom";
+
+jest.mock("../../../../services/api.service");
+
+describe("NewNote Component Integration", () => {
+  it("should render NewNote Component", () => {
+    render(
+      <BrowserRouter>
+        <NewNotes />
+      </BrowserRouter>
+    );
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Input Note Title")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Input Note Body")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("should can input title and body", () => {
+    render(
+      <BrowserRouter>
+        <NewNotes />
+      </BrowserRouter>
+    );
+    const titleInput = screen.getByPlaceholderText("Input Note Title");
+    const bodyInput = screen.getByPlaceholderText("Input Note Body");
+
+    fireEvent.change(titleInput, { target: { value: "Title Changed" } });
+    fireEvent.change(bodyInput, { target: { value: "Body Changed" } });
+
+    expect(titleInput.value).toBe("Title Changed");
+    expect(bodyInput.value).toBe("Body Changed");
+  });
+
+  it("should submit form and call apiService", async () => {
+    apiService.createNotes.mockResolvedValue({ status: "success" });
+    render(
+      <BrowserRouter>
+        <NewNotes />
+      </BrowserRouter>
+    );
+    const titleInput = screen.getByLabelText("Title");
+    const bodyInput = screen.getByLabelText("Note");
+    const submitButton = screen.getByText("Create Notes");
+
+    fireEvent.change(titleInput, { target: { value: "Test Title" } });
+    fireEvent.change(bodyInput, { target: { value: "Test Body" } });
+    fireEvent.click(submitButton);
+
+    expect(apiService.createNotes).toHaveBeenCalledWith({
+      title: "Test Title",
+      body: "Test Body",
+    });
+    expect(window.location.pathname).toBe("/");
+  });
+
+  test('handles API failure', async () => {
+    apiService.createNotes.mockResolvedValue({ status: 'error' });
+
+    render(
+      <BrowserRouter>
+        <NewNotes />
+      </BrowserRouter>
+    );
+
+    const titleInput = screen.getByLabelText('Title');
+    const bodyInput = screen.getByLabelText('Note');
+    const submitButton = screen.getByText('Create Notes');
+
+    fireEvent.change(titleInput, { target: { value: 'Test Title' } });
+    fireEvent.change(bodyInput, { target: { value: 'Test Body' } });
+    fireEvent.click(submitButton);
+
+    expect(apiService.createNotes).toHaveBeenCalledWith({
+      title: 'Test Title',
+      body: 'Test Body',
+    });
+    expect(titleInput).toBeInTheDocument();
+  });
+});
